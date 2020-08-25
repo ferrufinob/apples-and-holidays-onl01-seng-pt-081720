70152790cd9f3996f8b2a61b247336accbdbf8bd
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-25 05:05:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |seasons, holidays|
-    puts seasons.split(&:capitalize).join(' ')
+    puts seasons.capitalize
   # gsub("_", " ")
   # supplies.join(",")
      
