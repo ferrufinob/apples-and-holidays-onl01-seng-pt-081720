cf588d33ce6e0aade538c4d47ade874d5ad491ca
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-25 04:34:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
    holiday_hash[:winter].values.each do |supplies| 
-      supplies
+     puts"#{supplies"
 end  
 holiday_hash.flatten
 end
