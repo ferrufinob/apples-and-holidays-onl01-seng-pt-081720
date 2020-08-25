1bf421c3103cc992cb13caa9230681a8152b52d6
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-25 04:40:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,8 +82,8 @@ def all_holidays_with_bbq(holiday_hash)
 arr = []
 holiday_hash.each do |seasons, holidays|
    holidays.each do |holiday, supply|
-  if holiday.include?("BBQ")
-    arr << supply
+  if supply.include?("BBQ")
+    arr << holiday
 end
 end
 end
